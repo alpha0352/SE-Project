6155399e0c9d3c09103b00d537c88b8fbfb2a52d
alpha0352 <111266249+alpha0352@users.noreply.github.com>
2023-12-05 10:45:47 +0500

    Add files via upload

---
+++ sql/airplane.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select * from airplane
+LIMIT 0, 1000
+
+-- Date: 2023-12-04 12:58
+*/
+INSERT INTO `airplane` (`airplane_id`,`capacity`,`type_id`) VALUES ('AC-005',48,'P');
+INSERT INTO `airplane` (`airplane_id`,`capacity`,`type_id`) VALUES ('AC-342',25,'P');
---
+++ sql/airport.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: select * from airport
+LIMIT 0, 1000
+
+-- Date: 2023-12-04 13:01
+*/
+INSERT INTO `` (`airportid`,`iata`,`name`) VALUES (1,'LHE','Lahore');
+INSERT INTO `` (`airportid`,`iata`,`name`) VALUES (2,'KHI','Karachi');
+INSERT INTO `` (`airportid`,`iata`,`name`) VALUES (3,'ISB','Islamabad');
+INSERT INTO `` (`airportid`,`iata`,`name`) VALUES (4,'SKU','Skardu');
